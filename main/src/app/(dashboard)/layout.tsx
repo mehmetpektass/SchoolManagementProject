22c99fe4d2d4d1318ae07f685430afd6043c16c7
mehmetpektass <pektasmehmett1184@gmail.com>
2024-09-25 22:21:12 +0300

    started pages and layouts

---
+++ main/src/app/(dashboard)/layout.tsx
@@ -0,0 +1,28 @@
+import Menu from "@/components/Menu";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function DashboardLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+      
+        <div className="h-screen flex">
+          {/* Left*/}
+          <div className="w-[14%] md:w-[8%] lg:w-[19%] xl:w-[14%]">
+            <Link href='/' className="flex items-center justify-center lg:justify-start gap-2">
+            <Image src='/logo.png' alt="logo" width={32} height={32}/>
+            <span className="hidden lg:block">School Management</span>
+            </Link>
+            <Menu/>
+          </div>
+          {/*Right */}
+          <div className="w-[86%] md:w-[92%] lg:w-[81%] xl:w-[86%]  bg-[#F7F8FA]">
+            r
+          </div>
+        </div>
+      
+    );
+  }
--- main/src/components/Menu.tsx
+++ main/src/components/Menu.tsx
@@ -112,3 +112,27 @@ const menuItems = [
     ],
   },
 ];
+
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Menu = () => {
+  return (
+    <div className="mt-4 text-sm">
+      {menuItems.map((i) => (
+        <div className="flex flex-col gap-2" key={i.title}>
+          <span className="hidden lg:block text-gray-400 font-light my-4 ml-2">{i.title}</span>
+          {i.items.map((item) => (
+            <Link href={item.href} key={item.label} className="flex items-center justify-center lg:justify-start text-gray-500 gap-4 py-2 md:px-2 rounded-md hover:bg-lamaSkyLight">
+              <Image src={item.icon} alt={item.label} width={20} height={20} />
+              <span className="hidden lg:block">{item.label}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Menu;
