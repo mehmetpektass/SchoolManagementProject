43472f14e5d72e2cef8cecde24350f5ae4e98ab1
mehmetpektass <pektasmehmett1184@gmail.com>
2024-09-25 23:56:56 +0300

    started admin page

--- main/src/app/(dashboard)/admin/page.tsx
+++ main/src/app/(dashboard)/admin/page.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const AdminPage = () => {
   return (
-    <div>AdminPage</div>
+    <div className='flex p-4 gap-4 flex-col md:flex-row'>
+      <div className='w-full lg:w-2/3'>
+        l
+      </div>
+      <div className='w-full lg:w-1/3'>
+        r
+      </div>
+    </div>
   )
 }
 
--- main/src/components/Navbar.tsx
+++ main/src/components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
         </div>
         {/*Icons and user */}
         <div className='flex items-center gap-6 justify-end w-full'>
-            <div className='bg-whiterounded-full w-7 h-7 flex items-center justify-center cursor-pointer'>
+            <div className='bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer'>
                 <Image src='/message.png' alt='' height={20} width={20}/>
             </div>
             <div className='bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer relative'>
