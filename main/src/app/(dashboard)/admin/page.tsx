3df2e6d71559cd85a91826fbbfe37e026be1874a
mehmetpektass <pektasmehmett1184@gmail.com>
2024-09-26 23:08:24 +0300

    created finance  chart

--- main/src/app/(dashboard)/admin/page.tsx
+++ main/src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,6 @@
 import AttendanceChart from '@/components/AttendanceChart'
 import CountChart from '@/components/CountChart'
+import FinanceChart from '@/components/FinanceChart'
 import UserCard from '@/components/UserCard'
 import React from 'react'
 
@@ -20,6 +21,9 @@ const AdminPage = () => {
           <div className='w-full lg:w-2/3 h-[450px]'>
             <AttendanceChart/>
           </div>
+        </div>
+        <div className='w-full h-[500px]'>
+          <FinanceChart/>
         </div>
       </div>
       <div className='w-full lg:w-1/3'>
