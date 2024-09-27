68526d8dbdd214d8a3fff73d456cbb06df7b9247
mehmetpektass <pektasmehmett1184@gmail.com>
2024-09-27 12:07:46 +0300

    created calendar and events parts

--- main/src/app/(dashboard)/admin/page.tsx
+++ main/src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,6 @@
 import AttendanceChart from '@/components/AttendanceChart'
 import CountChart from '@/components/CountChart'
+import EventCalender from '@/components/EventCalender'
 import FinanceChart from '@/components/FinanceChart'
 import UserCard from '@/components/UserCard'
 import React from 'react'
@@ -27,7 +28,9 @@ const AdminPage = () => {
         </div>
       </div>
       <div className='w-full lg:w-1/3'>
-        r
+        <div className='w-full flex flex-col gap-8'>
+          <EventCalender/>
+        </div>
       </div>
     </div>
   )
--- main/src/app/(dashboard)/layout.tsx
+++ main/src/app/(dashboard)/layout.tsx
@@ -12,7 +12,7 @@ export default function DashboardLayout({
       
         <div className="h-screen flex">
           {/* Left*/}
-          <div className="w-[14%] md:w-[8%] lg:w-[19%] xl:w-[14%]">
+          <div className="w-[14%] md:w-[12%] lg:w-[14%] xl:w-[14%]">
             <Link href='/' className="flex items-center justify-center lg:justify-start gap-2">
             <Image src='/logo.png' alt="logo" width={32} height={32}/>
             <span className="hidden lg:block">School Management</span>
@@ -20,7 +20,7 @@ export default function DashboardLayout({
             <Menu/>
           </div>
           {/*Right */}
-          <div className="w-[86%] md:w-[92%] lg:w-[81%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+          <div className="w-[86%] md:w-[88%] lg:w-[86%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
             <Navbar/>
             {children}
           </div>
