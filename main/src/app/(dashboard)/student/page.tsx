f58d5ad5e674c445a98da5c04c052a38e3938094
mehmetpektass <pektasmehmett1184@gmail.com>
2024-09-27 15:17:33 +0300

    started student page

--- main/src/app/(dashboard)/student/page.tsx
+++ main/src/app/(dashboard)/student/page.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import Announcement from "@/components/Announcement";
+import EventCalender from "@/components/EventCalender";
+import React from "react";
 
 const StudentPage = () => {
   return (
-    <div>StudentPage</div>
-  )
-}
+    <div className="flex gap-4 p-4 flex-col lg:flex-row">
+      <div className="w-full lg:w-2/3 ">
+        <div className="h-full bg-white rounded-md">
+          <h1 className="text-xl font-semibold">Schedule(4A)</h1>
+        </div>
+      </div>
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <EventCalender />
+        <Announcement />
+      </div>
+    </div>
+  );
+};
 
-export default StudentPage
+export default StudentPage;
