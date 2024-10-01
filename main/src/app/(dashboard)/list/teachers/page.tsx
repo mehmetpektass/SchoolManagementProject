fd831f34187450d6bea4d435af507890c7784e76
mehmetpektass <pektasmehmett1184@gmail.com>
2024-10-01 16:16:54 +0300

    started teacher page

---
+++ main/src/app/(dashboard)/list/teachers/page.tsx
@@ -0,0 +1,25 @@
+import TableSearch from '@/components/TableSearch'
+import Image from 'next/image'
+import React from 'react'
+
+const TeacherListPage = () => {
+  return (
+    <div className='bg-white p-4 rounded-md flex-1 m-4 mt-0'>
+        <div className='flex items-center justify-between'>
+            <h1 className='hidden md:block text-lg font-semibold'>All Teachers</h1>
+            <div className='flex flex-col md:flex-row items-center gap-4 w-full md:w-auto'>
+                <TableSearch/>
+                <div>
+                    <button>
+                        <Image src='/filter.png' alt='' width={14} height={14} />
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div></div>
+        <div></div>
+    </div>
+  )
+}
+
+export default TeacherListPage
--- main/src/app/(dashboard)/parent/page.tsx
+++ main/src/app/(dashboard)/parent/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Announcement from "@/components/Announcement";
+import BigCalendar from "@/components/BigCalendar";
+import React from "react";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
 const ParentPage = () => {
   return (
-    <div>ParentPage</div>
-  )
-}
+    <div className="flex-1 p-4 flex gap-4 flex-col lg:flex-row">
+      <div className="w-full lg:w-2/3 ">
+        <div className="h-full bg-white rounded-md p-4">
+          <h1 className="text-xl font-semibold">Schedule (John Doe)</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+      
+        <Announcement />
+      </div>
+    </div>
+  );
+};
 
-export default ParentPage
+export default ParentPage;
