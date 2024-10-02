1ba157553a890102718aceacd3a28af729c69fec
mehmetpektass <pektasmehmett1184@gmail.com>
2024-10-02 13:42:42 +0300

    created all the list of users

--- main/src/components/Pagination.tsx
+++ main/src/components/Pagination.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Pagination = () => {
   return (
-    <div>
-        <button className='py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:cursor-not-allowed disabled:opacity-50'>Prev</button>
+    <div className='flex justify-between'>
+        <button className='py-2 px-4 mt-4 rounded-md bg-slate-200 text-xs font-semibold disabled:cursor-not-allowed disabled:opacity-50'>Prev</button>
         <div className='flex items-center justify-center gap-2'>
             <button className='px-2 rounded-md bg-lamaSky'>1</button>
             <button className='px-2 rounded-md'>2</button>
@@ -11,6 +11,7 @@ const Pagination = () => {
             ...
             <button className='px-2 rounded-md'>10</button>
         </div>
+        <button className='py-2 px-4 mt-4 rounded-md bg-slate-200 text-xs font-semibold disabled:cursor-not-allowed disabled:opacity-50'>Next</button>
     </div>
   )
 }
