ad60daa811c1b1ac50e34f01cb44b78862735680
mehmetpektass <pektasmehmett1184@gmail.com>
2024-10-11 12:46:04 +0300

    setted infos by the users

--- main/src/components/Navbar.tsx
+++ main/src/components/Navbar.tsx
@@ -1,8 +1,11 @@
 import { UserButton } from '@clerk/nextjs'
+import { currentUser } from '@clerk/nextjs/server'
 import Image from 'next/image'
 import React from 'react'
 
-const Navbar = () => {
+const Navbar = async () => {
+    const user = await currentUser();
+    const role = user?.publicMetadata.role as string;
   return (
     <div className='flex items-center justify-between p-4'>
         {/*Search Bar */}
@@ -21,7 +24,7 @@ const Navbar = () => {
             </div>
             <div className='flex flex-col'>
                 <span className='text-xs leading-3 font-medium'>Alan Doe</span>
-                <span className='text-[10px] text-right text-gray-500'>Admin</span>
+                <span className='text-[10px] text-right text-gray-500'>{role}</span>
             </div>
             <div>
                <UserButton/>
